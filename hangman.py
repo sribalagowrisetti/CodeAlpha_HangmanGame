6b6c139f8e7a3cb973389aff78cb7ce21959e981
sribalagowrisetti <sribalagowrisetti@gmail.com>
2026-06-16 11:16:29 +0530

    Add Hangman Game source code

---
+++ hangman.py
@@ -0,0 +1,44 @@
+import random
+
+words = ["python", "apple", "house", "water", "music"]
+
+word = random.choice(words)
+
+guessed_letters = []
+wrong_guesses = 0
+max_wrong_guesses = 6
+
+print("Welcome to Hangman!")
+
+while wrong_guesses < max_wrong_guesses:
+
+    display_word = ""
+
+    for letter in word:
+        if letter in guessed_letters:
+            display_word += letter + " "
+        else:
+            display_word += "_ "
+
+    print("\nWord:", display_word)
+
+    if "_" not in display_word:
+        print("Congratulations! You guessed the word:", word)
+        break
+
+    guess = input("Enter a letter: ").lower()
+
+    if guess in guessed_letters:
+        print("You already guessed that letter.")
+    elif guess in word:
+        guessed_letters.append(guess)
+        print("Correct!")
+    else:
+        guessed_letters.append(guess)
+        wrong_guesses += 1
+        print("Wrong guess!")
+        print("Remaining chances:", max_wrong_guesses - wrong_guesses)
+
+if wrong_guesses == max_wrong_guesses:
+    print("\nGame Over!")
+    print("The word was:", word)
